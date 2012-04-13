f297cddfda0b76b6fa8789b06023db14603b2a67
Eric Blade <blade.eric@gmail.com>
2012-04-13 19:29:31 -0400

    sadkfj

--- mainApp/source/gvoice.js
+++ mainApp/source/gvoice.js
@@ -709,7 +709,7 @@ enyo.kind({
         }
     
         var runcount = parseInt(prefs.get("runcount"));
-        enyo.log("runcount=", runcount, "reviewed=", prefs.get("reviewed"));
+        enyo.log(prefs.get("runcount"), "runcount=", runcount, "reviewed=", prefs.get("reviewed"));
         if(runcount < 10 && !prefs.get("reviewed"))
         {
             this.$.ReviewPopup.open();
