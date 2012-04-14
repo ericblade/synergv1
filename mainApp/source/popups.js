20c70a270bb6b7a0b99e459de4e002142b16484d
Eric Blade <blade.eric@gmail.com>
2012-04-13 20:12:24 -0400

    sdkfjsdf

--- mainApp/source/popups.js
+++ mainApp/source/popups.js
@@ -1,7 +1,7 @@
 enyo.kind({
-    name: "ReviewPopup", kind: "Popup", components:
+    name: "ReviewPopup", kind: "Popup", height: "80%", components:
         [
-            { kind: "VFlexBox", height: "80%",  components:
+            { kind: "VFlexBox",  components:
                 [
                     { kind: "Scroller", flex: 1, style: "background-color: #4185D3;", components:
                         [
