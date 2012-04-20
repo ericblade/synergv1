b056eaf4433169ed5b2c34b032b85c46e3501b56
Eric Blade <blade.eric@gmail.com>
2012-04-20 01:40:35 -0400

    sd;fjksdf

--- noWindow/applaunch.js
+++ noWindow/applaunch.js
@@ -44,12 +44,12 @@ enyo.kind({
 			{
 				this.log("deleting", inResponse.results[x]["_id"]);
 				this.$.dbDel.call({ ids: [ inResponse.results[x]["_id"] ] });
-				if(enyo.application.mainApp)
+				/*if(enyo.application.mainApp)
 				{
 					this.log(enyo.application.mainApp, "spooling message", inResponse.results[x]);
 					// multiple recipients can be specified in the incoming array!! make sure we handle
 					enyo.application.mainApp.$.outbox.queueMessage(inResponse.results[x].to[0].addr, inResponse.results[x].messageText);
-				}
+				}*/
 			}
 			// queue automatically sends when online or sets timer when not
 			//if(enyo.application.mainApp)
