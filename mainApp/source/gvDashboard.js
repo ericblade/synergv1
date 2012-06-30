35f436ab7cc0f9cb0e0ed9e922f8d5633a1d4755
Eric Blade <blade.eric@gmail.com>
2012-06-29 20:22:20 -0400

    sdjfsdf

--- mainApp/source/gvDashboard.js
+++ mainApp/source/gvDashboard.js
@@ -10,7 +10,7 @@ enyo.kind({
 		if(this.layers.length) {
 			var params = {layers:this.layers, docPath:document.location.pathname, dashboardId:this.dashboardId};
 			if(!windowValid) {
-				var attributes = {webosDragMode:"manual", window:"dashboard", _enyoOpener:window, "doubleheightdash":false};
+				var attributes = {webosDragMode:"manual", window:"dashboard", _enyoOpener:window, "doubleheightdash":true};
 				if(this.smallIcon) {
 					attributes.icon = this.smallIcon;
 				}
