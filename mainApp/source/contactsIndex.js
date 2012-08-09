4be044df334c9400c4ee751fdb479e3349fac2c4
Eric Blade <blade.eric@gmail.com>
2012-08-09 16:35:27 -0400

    Revert "acceleration changes"
    This reverts commit 595027d6bb38a69dd66f733983363b8b593d0b93.
    
    Conflicts:
    
    	mainApp/source/contactDetail.js

--- mainApp/source/contactsIndex.js
+++ mainApp/source/contactsIndex.js
@@ -4,7 +4,7 @@ enyo.kind( {
     autoHorizontal: false,
     horizontal: false,
     autoVertical: true,
-    accelerated: useAccelerated(),
+    accelerated: true,
     events:
         { "onContactSelected":"", },
     components:
@@ -16,7 +16,7 @@ enyo.kind( {
                 ]
             },*/
             
-            { kind: "VirtualRepeater", flex: 1, onSetupRow: "getContactsIndexItem", onclick: "indexClick", accelerated: useAccelerated(), components:
+            { kind: "VirtualRepeater", flex: 1, onSetupRow: "getContactsIndexItem", onclick: "indexClick", accelerated: true, components:
                 [
                     { kind: "Item", layoutKind: "HFlexLayout", onclick: "selected", components:
                         [
