cd851360e24b345dca905e6f9f4ece33bcbf27ce
Eric Blade <blade.eric@gmail.com>
2012-12-30 01:31:19 -0500

    blah

--- mainApp/source/gvoice.js
+++ mainApp/source/gvoice.js
@@ -2196,10 +2196,10 @@ enyo.kind({
     },
     playVoicemail: function(msgid)
     {
-        console.log("playVoicemail: msgid=", msgid);
         if(!msgid) {
             msgid = enyo.application.mainApp.selectedID;
         }
+        console.log("playVoicemail: msgid=", msgid);
         if(Platform.isWebOS())
         {
             if(Platform.platformVersion >= 2)
